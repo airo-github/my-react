8d912e99fe8ce84e20aba41af4d33aa85a1aee4e
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-11-03 21:32:11 +0900

    3-1-2

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './class.css';
+import MyHello from './chap03/MyHello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <a className='hoge'>WINGSプロジェクト</a>
+  <MyHello myName='鈴木' />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
