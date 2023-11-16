9906fd3dc6bc30e156b6477469d7c2ab685425a8
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-11-16 21:58:28 +0900

    Add: スタイルの適用

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './class.css';
-import books from './chap03/books'
-import ForSort from './chap03/ForSort';
+import SelectStyle from './chap03/SelectStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ForSort src={books} />
+  <SelectStyle mode='light' />
 );
 
 reportWebVitals();
